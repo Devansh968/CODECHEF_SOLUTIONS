7bee74405e1b866b59d23741b0d27f699e2c5707
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2022-07-13 22:11:22 +0530

    Add files via upload

---
+++ Chef on date/Chef on date.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	  int  x,y;
+	  
+	  cin>>x>>y;
+	  
+	  if(x==y   ||  x>y)
+	  {
+	     cout<<"YES"<<endl;
+	  }
+	  else{
+	     cout<<"NO"<<endl;
+	  }
+	}
+	return 0;
+}
